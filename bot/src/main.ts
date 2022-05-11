72bab1b93a8d10906feee996036fdcb9e37b41c1
xirurgx3d <xirurgx3d@gmail.com>
2022-05-11 10:52:31 +0300

    Update main.ts

--- bot/src/main.ts
+++ bot/src/main.ts
@@ -4,7 +4,7 @@ config();
 import * as TelegramBot from "node-telegram-bot-api";
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import { generateMessage } from "./services/generateMessage/generateMessage.service";
+import { generateMessage, messageReserveTable } from "./services/generateMessage/generateMessage.service";
 import { OrganizationRepository } from "./repository/organization.repository";
 import { connection } from "./db/connection";
 const app = express();
@@ -14,7 +14,7 @@ const app = express();
 
 // new 5298758359:AAHwrFD23e_RBUSXmimi72p5wI8MZtLYGTg
 
-const bot = new TelegramBot("5298758359:AAHwrFD23e_RBUSXmimi72p5wI8MZtLYGTg", { polling: true });
+const bot = new TelegramBot("1858418208:AAHbGAeh6mG-XYsASrs7f_CRgxt4OMnmduw", { polling: true });
 
 app.use(bodyParser());
 
@@ -40,6 +40,28 @@ app.post("/sendDuplicate/:organizationId", async (req, res) => {
     await bot.sendMessage(organizationDoc.chat, message);
 
     res.status(200).json({ haveProblem: false, message: "Message is send" });
+});
+
+
+app.post("/reserveTable/:organizationId", async (req, res) => {
+  const organization = req.params.organizationId;
+  const body = req.body;
+  const organizationDoc = await OrganizationRepository.getOne(organization);
+
+  console.log(body,organization,organizationDoc)
+  if (!organizationDoc) {
+      return res.status(200).json({
+          haveProblem: true,
+          message: "Organization not found"
+      });
+  }
+  
+
+  const message = messageReserveTable(body);
+
+  await bot.sendMessage(organizationDoc.chat, message);
+
+  res.status(200).json({ haveProblem: false, message: "Message is send" });
 });
 
 bot.onText(/\/reg (.+)/i, async (msg, match) => {
