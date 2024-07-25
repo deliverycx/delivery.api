757abc0f684f9a9784d3b79a3feb27988a7ace1d
khinkalich <xirurgx3d@gmail.com>
2024-07-25 21:36:17 +0300

    доставка 1110

--- server/src/services/delivery/delivery.service.ts
+++ server/src/services/delivery/delivery.service.ts
@@ -7,127 +7,127 @@ import { IDeliveryPrices, IDeliveryService } from "./delivery.abstract";
 
 @Injectable()
 export class DeliveryService implements IDeliveryService {
-	protected iiko:IIkoAxios
-    constructor(
-			private readonly cartRepository: ICartRepository,
-			) {
-				this.iiko = new IIkoAxios()
+	protected iiko: IIkoAxios
+	constructor(
+		private readonly cartRepository: ICartRepository,
+	) {
+		this.iiko = new IIkoAxios()
+	}
+
+	private async deliveryPriceCalculating(
+		price: number,
+		orderType: OrderTypesEnum
+	): Promise<number> {
+		if (orderType === OrderTypesEnum.PICKUP || orderType === OrderTypesEnum.ONSPOT) {
+			return 0;
+		}
+
+		return price < 1100 ? 200 : 0;
+	}
+	private async cartPriceCalculating(userId: UniqueId, discount?: number): Promise<number> {
+		let totalPrice = await this.cartRepository.calc(userId);
+		return discount ? totalPrice - discount : totalPrice
+	}
+
+	public async calculatingPrices(
+		userId: UniqueId,
+		orderType: OrderTypesEnum,
+		organization?: string,
+		discount?: number
+	): Promise<IDeliveryPrices> {
+		const carts = await this.cartRepository.getAllDisc(userId)
+
+
+		const { count, min } = validationHIdiscount(carts)
+
+
+		const forhach = validationHachapuriFO(carts)
+
+		const totalPrice = await this.cartPriceCalculating(userId, min);
+		const deliveryPrice = await this.deliveryPriceCalculating(
+			totalPrice,
+			orderType
+		);
+
+
+		let deltaPrice = 0;
+
+		if (orderType === OrderTypesEnum.COURIER) {
+			deltaPrice = 1100 - totalPrice < 0 ? 0 : 1100 - totalPrice;
+		}
+
+		if (count !== 0) {
+			return {
+				deliveryPrice,
+				totalPrice: totalPrice + deliveryPrice,
+				deltaPrice: deltaPrice !== 0 ? deltaPrice : 0,
+				fullPrice: totalPrice,
+				discounts: {
+					forhach: forhach
+				}
+			};
+		}
+
+
+
+		return {
+			deliveryPrice,
+			totalPrice: totalPrice + deliveryPrice,
+			deltaPrice,
+			fullPrice: totalPrice,
+			discounts: {
+				forhach: forhach
 			}
+		};
+	}
 
-    private async deliveryPriceCalculating(
-        price: number,
-        orderType: OrderTypesEnum
-    ): Promise<number> {
-        if (orderType === OrderTypesEnum.PICKUP || orderType === OrderTypesEnum.ONSPOT) {
-            return 0;
-        }
 
-        return price < 850 ? 150 : 0;
-    }
-    private async cartPriceCalculating(userId: UniqueId,discount?:number): Promise<number> {
-        let totalPrice = await this.cartRepository.calc(userId);
-        return discount ? totalPrice - discount : totalPrice
-    }
 
-    public async calculatingPrices(
-        userId: UniqueId,
-        orderType: OrderTypesEnum,
-				organization?:string,
-				discount?:number
-    ): Promise<IDeliveryPrices> {
-				const carts = await this.cartRepository.getAllDisc(userId)
+
+	async discountDozenServise(userId: UniqueId, organization: string) {
+		const carts = await this.cartRepository.getAllDisc(userId)
+		const cartValid = validationHIdiscount(carts)
+
+		/*
+		if(organization){
+			const data = await this.iiko.discontList(
+				{
+					organization,
+					order:{
+						items:carts
+					}
+				})
 				
+				const dozen = data.loyatyResult.programResults.filter((val:any)=> val.name.indexOf('12-е хинкали в подарок') >= 0 && val)[0];
 				
-				const {count,min} = validationHIdiscount(carts)
-
-				  
-				const forhach = validationHachapuriFO(carts)
-				
-        const totalPrice = await this.cartPriceCalculating(userId,min);
-        const deliveryPrice = await this.deliveryPriceCalculating(
-            totalPrice,
-            orderType
-        );
-				
-				
-        let deltaPrice = 0;
-
-        if (orderType === OrderTypesEnum.COURIER) {
-            deltaPrice = 850 - totalPrice < 0 ? 0 : 850 - totalPrice;
-        }
-
-				if(count !== 0){
-					return {
-            deliveryPrice,
-            totalPrice: totalPrice + deliveryPrice,
-            deltaPrice:deltaPrice !== 0 ? deltaPrice : 0,
-						fullPrice:totalPrice,
-						discounts:{
-							forhach:forhach
-						}
-        	};
+				if(dozen.discounts.length){
+					const {discountSum} = dozen.discounts[0]
+					return{
+						discountDozen:discountSum
+					}
 				}
-
 				
-
-        return {
-            deliveryPrice,
-            totalPrice: totalPrice + deliveryPrice,
-            deltaPrice,
-						fullPrice:totalPrice,
-						discounts:{
-							forhach:forhach
-						}
-        };
-    }
-
-
-	
-
-		async discountDozenServise(userId: UniqueId,organization:string){
-				const carts = await this.cartRepository.getAllDisc(userId)
-				const cartValid = validationHIdiscount(carts)
-
-				/*
-				if(organization){
-					const data = await this.iiko.discontList(
-						{
-							organization,
-							order:{
-								items:carts
-							}
-						})
-						
-						const dozen = data.loyatyResult.programResults.filter((val:any)=> val.name.indexOf('12-е хинкали в подарок') >= 0 && val)[0];
-						
-						if(dozen.discounts.length){
-							const {discountSum} = dozen.discounts[0]
-							return{
-								discountDozen:discountSum
-							}
-						}
-						
-				}
-				*/
-				
-				return {
-					discountDozen:0
-				}
 		}
-		async deliveryZone(organization:string){
-			const {coordinates} = await this.iiko.getDeliveryZones({
-				organizationIds:[organization]
-			})
-			const mass = coordinates.reduce((acc:number[][],val,index)=>{
-				acc.push([Number(val.latitude),Number(val.longitude)])
-				
-				return acc
-			},[])
+		*/
 
-			return mass
+		return {
+			discountDozen: 0
 		}
+	}
+	async deliveryZone(organization: string) {
+		const { coordinates } = await this.iiko.getDeliveryZones({
+			organizationIds: [organization]
+		})
+		const mass = coordinates.reduce((acc: number[][], val, index) => {
+			acc.push([Number(val.latitude), Number(val.longitude)])
+
+			return acc
+		}, [])
+
+		return mass
+	}
 
 
-		
-		
+
+
 }
