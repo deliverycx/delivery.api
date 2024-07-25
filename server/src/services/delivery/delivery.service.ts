75efd05063e50df247cdb9b30474cd8897d72e71
khinkalich <xirurgx3d@gmail.com>
2024-07-25 21:37:45 +0300

    доставка 1100

--- server/src/services/delivery/delivery.service.ts
+++ server/src/services/delivery/delivery.service.ts
@@ -7,97 +7,97 @@ import { IDeliveryPrices, IDeliveryService } from "./delivery.abstract";
 
 @Injectable()
 export class DeliveryService implements IDeliveryService {
-	protected iiko:IIkoAxios
-    constructor(
-			private readonly cartRepository: ICartRepository,
-			) {
-				this.iiko = new IIkoAxios()
-			}
+	protected iiko: IIkoAxios
+	constructor(
+		private readonly cartRepository: ICartRepository,
+	) {
+		this.iiko = new IIkoAxios()
+	}
 
-    private async deliveryPriceCalculating(
-        price: number,
-        orderType: OrderTypesEnum
-    ): Promise<number> {
-        if (orderType === OrderTypesEnum.PICKUP) {
-            return 0;
-        }
-
-        return price < 850 ? 150 : 0;
-    }
-    private async cartPriceCalculating(userId: UniqueId,discount?:number): Promise<number> {
-        let totalPrice = await this.cartRepository.calc(userId);
-        return discount ? totalPrice - discount : totalPrice
-    }
-
-    public async calculatingPrices(
-        userId: UniqueId,
-        orderType: OrderTypesEnum,
-				organization?:string,
-				discount?:number
-    ): Promise<IDeliveryPrices> {
-				const carts = await this.cartRepository.getAllDisc(userId)
-				const {count,min} = validationHIdiscount(carts)
-
-        const totalPrice = await this.cartPriceCalculating(userId,min);
-        const deliveryPrice = await this.deliveryPriceCalculating(
-            totalPrice,
-            orderType
-        );
-
-
-				
-				
-				
-				
-        let deltaPrice = 0;
-
-        if (orderType === OrderTypesEnum.COURIER) {
-            deltaPrice = 850 - totalPrice < 0 ? 0 : 850 - totalPrice;
-        }
-
-				if(count !== 0){
-					return {
-            deliveryPrice,
-            totalPrice: totalPrice + deliveryPrice,
-            deltaPrice:deltaPrice !== 0 ? deltaPrice : 0
-        	};
-				}
-
-        return {
-            deliveryPrice,
-            totalPrice: totalPrice + deliveryPrice,
-            deltaPrice
-        };
-    }
-
-		async discountDozenServise(userId: UniqueId,organization:string){
-				const carts = await this.cartRepository.getAllDisc(userId)
-				const cartValid = validationHIdiscount(carts)
-				/*
-				if(organization){
-					const data = await this.iiko.discontList(
-						{
-							organization,
-							order:{
-								items:carts
-							}
-						})
-						
-						const dozen = data.loyatyResult.programResults.filter((val:any)=> val.name.indexOf('12-е хинкали в подарок') >= 0 && val)[0];
-						
-						if(dozen.discounts.length){
-							const {discountSum} = dozen.discounts[0]
-							return{
-								discountDozen:discountSum
-							}
-						}
-						
-				}
-				*/
-				
-				return {
-					discountDozen:0
-				}
+	private async deliveryPriceCalculating(
+		price: number,
+		orderType: OrderTypesEnum
+	): Promise<number> {
+		if (orderType === OrderTypesEnum.PICKUP) {
+			return 0;
 		}
-		
+
+		return price < 1100 ? 200 : 0;
+	}
+	private async cartPriceCalculating(userId: UniqueId, discount?: number): Promise<number> {
+		let totalPrice = await this.cartRepository.calc(userId);
+		return discount ? totalPrice - discount : totalPrice
+	}
+
+	public async calculatingPrices(
+		userId: UniqueId,
+		orderType: OrderTypesEnum,
+		organization?: string,
+		discount?: number
+	): Promise<IDeliveryPrices> {
+		const carts = await this.cartRepository.getAllDisc(userId)
+		const { count, min } = validationHIdiscount(carts)
+
+		const totalPrice = await this.cartPriceCalculating(userId, min);
+		const deliveryPrice = await this.deliveryPriceCalculating(
+			totalPrice,
+			orderType
+		);
+
+
+
+
+
+
+		let deltaPrice = 0;
+
+		if (orderType === OrderTypesEnum.COURIER) {
+			deltaPrice = 1100 - totalPrice < 0 ? 0 : 1100 - totalPrice;
+		}
+
+		if (count !== 0) {
+			return {
+				deliveryPrice,
+				totalPrice: totalPrice + deliveryPrice,
+				deltaPrice: deltaPrice !== 0 ? deltaPrice : 0
+			};
+		}
+
+		return {
+			deliveryPrice,
+			totalPrice: totalPrice + deliveryPrice,
+			deltaPrice
+		};
+	}
+
+	async discountDozenServise(userId: UniqueId, organization: string) {
+		const carts = await this.cartRepository.getAllDisc(userId)
+		const cartValid = validationHIdiscount(carts)
+		/*
+		if(organization){
+			const data = await this.iiko.discontList(
+				{
+					organization,
+					order:{
+						items:carts
+					}
+				})
+				
+				const dozen = data.loyatyResult.programResults.filter((val:any)=> val.name.indexOf('12-е хинкали в подарок') >= 0 && val)[0];
+				
+				if(dozen.discounts.length){
+					const {discountSum} = dozen.discounts[0]
+					return{
+						discountDozen:discountSum
+					}
+				}
+				
+		}
+		*/
+
+		return {
+			discountDozen: 0
+		}
+	}
+
 }
