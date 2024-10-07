b3c6565a1a001978abe8e8d32a0e0307fe7b3ff4
khinkalich <xirurgx3d@gmail.com>
2024-10-07 11:24:32 +0300

    Update auth.guard.ts

--- server/src/guards/auth.guard.ts
+++ server/src/guards/auth.guard.ts
@@ -1,10 +1,10 @@
 
 import {
 	BadRequestException,
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
+	CanActivate,
+	ExecutionContext,
+	Injectable,
+	UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as argon2 from 'argon2';
@@ -15,55 +15,55 @@ import { UserUsecase } from 'src/components/user/usecases/user.usecase';
 export class AuthJWTGuard implements CanActivate {
 	constructor(
 		private jwtService: JwtService,
-		private userService:UserUsecase
-		) {}
-	
-		async canActivate(context: ExecutionContext): Promise<boolean> {
-			const request = context.switchToHttp().getRequest();
-			const token = this.extractTokenFromHeader(request);
+		private userService: UserUsecase
+	) { }
 
-			if (!token) {
-				return false
-			}
-				
-      try {
-				const payload = await this.jwtService.verifyAsync(token.token, {
-					secret: process.env.SESSION_SECRET,
-				});
-				if(payload){
-					this.refreshToken(token.refreshtoken,request.body.id)
-				}
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+		const request = context.switchToHttp().getRequest();
+		const token = this.extractTokenFromHeader(request);
 
-			} catch {
-				throw new UnauthorizedException();
-			}
-			return true;
-    }
-
-		private extractTokenFromHeader(request: Request) {
-			let data = request.cookies["auth-cookie"];
-			if(!data){
-				return null;
-			}
-			return {
-				token:data.token,
-				refreshtoken:data.refreshToken
-			} 
+		if (!token) {
+			return false
 		}
 
-		private async refreshToken(token:string,id:string){
-			const user = await this.userService.getUser(id)
-				if(!user){
-					console.log('token expired',user,id);
-					
-            //throw new BadRequestException('token expired');
-        }
-			const tokenMatches = await argon2.verify(user.getToken,token)
-				if(!tokenMatches){
-					console.log('Matches invalid refresh token',user.getToken,token);
-					
-					
-					//throw new BadRequestException('Matches invalid refresh token');
-				}
+		try {
+			const payload = await this.jwtService.verifyAsync(token.token, {
+				secret: process.env.SESSION_SECRET,
+			});
+			if (payload) {
+				this.refreshToken(token.refreshtoken, request.body.id)
+			}
+
+		} catch {
+			throw new UnauthorizedException();
 		}
+		return true;
+	}
+
+	private extractTokenFromHeader(request: Request) {
+		let data = request.cookies["auth-cookie"];
+		if (!data) {
+			return null;
+		}
+		return {
+			token: data.token,
+			refreshtoken: data.refreshToken
+		}
+	}
+
+	private async refreshToken(token: string, id: string) {
+		const user = await this.userService.getUser(id)
+		if (!user) {
+			//console.log('token expired',user,id);
+
+			//throw new BadRequestException('token expired');
+		}
+		const tokenMatches = await argon2.verify(user.getToken, token)
+		if (!tokenMatches) {
+			console.log('Matches invalid refresh token', user.getToken, token);
+
+
+			//throw new BadRequestException('Matches invalid refresh token');
+		}
+	}
 }
