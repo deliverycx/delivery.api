cb2b53caa12ffd41ea83e00b0666f2765e99d98c
khinkalich <xirurgx3d@gmail.com>
2024-12-24 11:48:20 +0300

    тест времени

--- server/src/components/city/controllers/city.controller.ts
+++ server/src/components/city/controllers/city.controller.ts
@@ -1,61 +1,76 @@
-import { Controller, Get, HttpException, HttpStatus, Query, Req, Res,Headers } from "@nestjs/common";
+import { Controller, Get, HttpException, HttpStatus, Query, Req, Res, Headers } from "@nestjs/common";
 import { CityUsecase } from "../usecases/city.usecase";
 import { ApiResponse, ApiTags } from "@nestjs/swagger";
 import { CityEntity } from "../entities/city.entity";
 import { CityQueryDTO } from "../dto/cityQuery.dto";
-import { Response,Request } from "express";
+import { Response, Request } from "express";
 
 @ApiTags("City endpoints")
 @Controller("city")
 export class CityController {
-    constructor(private readonly cityUsecase: CityUsecase) {}
+	constructor(private readonly cityUsecase: CityUsecase) { }
 
-    @ApiResponse({
-        status: 200,
-        type: [CityEntity]
-    })
-    @Get("all")
-    async getAll(
-        @Query()
-        query: CityQueryDTO
-    ) {
-        const result = this.cityUsecase.getAll(
-            query.search ? query.search : ""
-        );
+	@ApiResponse({
+		status: 200,
+		type: [CityEntity]
+	})
+	@Get("all")
+	async getAll(
+		@Query()
+		query: CityQueryDTO
+	) {
+		const result = this.cityUsecase.getAll(
+			query.search ? query.search : ""
+		);
 
-        //throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
-        return result;
-    }
+		const trueDate = new Date();
+		function formatDate(date: any) {
+			var dd: any = date.getDate();
+			if (dd < 10) dd = '0' + dd;
 
-		@Get("buid")
-    async getBuId(
-        @Query()
-        query: CityQueryDTO
-    ) {
-        const result = this.cityUsecase.getBuId(
-            query.id
-        );
-				
-        //throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
-        return result;
-    }
+			var mm: any = date.getMonth() + 1;
+			if (mm < 10) mm = '0' + mm;
 
-		@Get("webhook_all")	
-    async webhookgetAll(
-				@Headers('authorization') headers,
-				@Req() req:Request,
-				@Res() response: Response,
-    ) {
+			var yy: any = date.getFullYear() % 100;
+			if (yy < 10) yy = '0' + yy;
 
-				const refreshToken = headers.replace('Bearer', '').trim();
+			return dd + '.' + mm + '.' + yy;
+		}
+		console.log(formatDate(trueDate));
 
-				const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBcGlMb2dpbklkIjoiYzZjOWJjOGUtYzYxOC00MzdmLTgwMDgtYTc5NWY5Nzk4ZmM5IiwibmJmIjoxNjgxMjg2ODI3LCJleHAiOjE2ODEyOTA0MjcsImlhdCI6MTY4MTI4NjgyNywiaXNzIjoiaWlrbyIsImF1ZCI6ImNsaWVudHMifQ.ZAJR-BmWNs4S9Eqx7gL7jviJ5P0nVAbBwFVI99-YI88'
-				if(refreshToken !== token){
-					response.status(401).json({});
-				} 
-        const result = await this.cityUsecase.getAll("");
-				
-        //throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
-        response.status(200).json(result);
-    }
+		//throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+		return result;
+	}
+
+	@Get("buid")
+	async getBuId(
+		@Query()
+		query: CityQueryDTO
+	) {
+		const result = this.cityUsecase.getBuId(
+			query.id
+		);
+
+		//throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+		return result;
+	}
+
+	@Get("webhook_all")
+	async webhookgetAll(
+		@Headers('authorization') headers,
+		@Req() req: Request,
+		@Res() response: Response,
+	) {
+
+		const refreshToken = headers.replace('Bearer', '').trim();
+
+		const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBcGlMb2dpbklkIjoiYzZjOWJjOGUtYzYxOC00MzdmLTgwMDgtYTc5NWY5Nzk4ZmM5IiwibmJmIjoxNjgxMjg2ODI3LCJleHAiOjE2ODEyOTA0MjcsImlhdCI6MTY4MTI4NjgyNywiaXNzIjoiaWlrbyIsImF1ZCI6ImNsaWVudHMifQ.ZAJR-BmWNs4S9Eqx7gL7jviJ5P0nVAbBwFVI99-YI88'
+		if (refreshToken !== token) {
+			response.status(401).json({});
+		}
+		const result = await this.cityUsecase.getAll("");
+
+		//throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+		response.status(200).json(result);
+	}
 }
