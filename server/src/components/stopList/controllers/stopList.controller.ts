fa59a317bc33c69d11de0ffac0cfb3adb0fca046
khinkalich <xirurgx3d@gmail.com>
2025-11-05 19:41:55 +0300

    Update stopList.controller.ts

--- server/src/components/stopList/controllers/stopList.controller.ts
+++ server/src/components/stopList/controllers/stopList.controller.ts
@@ -1,17 +1,19 @@
 import { Controller, Get, Post, Query } from "@nestjs/common";
 import { StopListUsecase } from "../usecases/stopList.usecase";
+import axios from "axios";
 
 @Controller("stoplist")
 export class StopListController {
-	constructor(private readonly stopListUsecase: StopListUsecase) {}
+	constructor(private readonly stopListUsecase: StopListUsecase) { }
 
 	@Get("getStopList")
-    async getAllById(
-			@Query() query: {organizationId:string}
-			) {
+	async getAllById(
+		@Query() query: { organizationId: string }
+	) {
 
-        const result = await this.stopListUsecase.getAll(query.organizationId);
+		//const result = await this.stopListUsecase.getAll(query.organizationId);
+		const { data } = await axios.get(`https://cxfranhizetest.ru/api/menuWeb/stoplist/${query.organizationId}`)
 
-        return result
-    }
+		return data
+	}
 }
